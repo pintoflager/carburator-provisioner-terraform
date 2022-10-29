0bbd587b0d0fbcc7c7f1e1eb9c74a60a8ba971e8
pintoflager <jaakko0606@gmail.com>
2022-10-29 13:36:44 -0500

    ready to try provisioner register

--- providers/hetzner/project/create.sh
+++ providers/hetzner/project/create.sh
@@ -1,30 +1,54 @@
 #!/bin/bash
 
-# TODO: pass environment variables to script before exec.
+# TODO: following env should be present
+# PROVIDER_NAME = "hetzner"
+# PROVIDER_DEFAULT = true
+# PROVIDER_SECRET_0 = hetzner_cloud_apikey
+# PROVIDER_TARGET = "production"
+# PROVIDER_PATH = "/home/..../providers/service/{name}"
 
-# TODO: try to make as generic as possible.
+# PROVISIONER_NAME = "terraform"
+# PROVISIONER_BIN = "\"terraform\""
+# PROVISIONER_default = true
+# PROVISIONER_retry_times = 3
+# PROVISIONER_retry_interval = 10
+# PROVISIONER_boot_wait = 20
+# PROVISIONER_target = "production"
+# PROVISIONER_PROVIDER_PATH = "/home/..../provisioners/{name}/providers{service_provider}"
+# PROVISIONER_HOME = "/home/..../provisioners/{name}"
 
-project="$1" pr_id="$2" project_json rundir;
+# TODO: not needed here but every script execution should include envs from project.toml
+project="$1"
+pr_id="$2"
 
-rundir="$PWD/$pr_id"
-project_json="$rundir/$project.json"
-
-# Register project with hetzner. Adds ssh key for project root user.
+# Register project with hetzner. Adds ssh key for project root.
+# TODO: these are fixed now. Needs path to project private and public dirs.
 key_path=$(get-env EXECUTOR_SSH_KEY "$PWD/.env")
 keypub_path=$(get-env EXECUTOR_SSH_KEY_PUB "$PWD/.env")
 comm=$(awk '{print $3}' "$keypub_path")
-tf_context="$rundir/.tf-$project"
 
-# Create terraform data dir before hand to avoid warnings.
-mkdir -p "$rundir/.terraform"
+# Create terraform data dir beforehand to avoid warnings.
+mkdir -p "$PROVISIONER_HOME/.terraform"
 
-export TF_VAR_api_token="$cloud_token"
-export TF_DATA_DIR="$rundir/.terraform"
-export TF_PLUGIN_CACHE_DIR="$rundir/.terraform"
+###
+# Get API token from secrets or fail early.
+#
+token=$(carburator get secret "$PROVIDER_SECRET_0") || exit 1
+if [[ -z $token ]]; then
+	carburator fn paint red \
+		"Hetzner API token from secret came back empty. Unable to proceed"
+fi
+
+export TF_DATA_DIR="$PROVISIONER_HOME/.terraform"
+export TF_PLUGIN_CACHE_DIR="$PROVISIONER_HOME/.terraform"
+
+export TF_VAR_api_token="$token"
 export TF_VAR_sshkey="{name = \"$comm\", path = \"$key_path\"}"
+# TODO: project.toml > env
 export TF_VAR_identifier="$project"
 
-terraform -chdir="$tf_context" init
-terraform -chdir="$tf_context" apply -auto-approve
-terraform -chdir="$tf_context" output -json > "$project_json"
+terraform -chdir="$PROVISIONER_PROVIDER_PATH/.tf-project" init
+terraform -chdir="$PROVISIONER_PROVIDER_PATH/.tf-project" apply -auto-approve
+terraform -chdir="$PROVISIONER_PROVIDER_PATH/.tf-project" output -json > \
+	"$PROVISIONER_PATH/project.json"
 
